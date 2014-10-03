ef904828efd12b8b67acf7ddadbdf86168a8025e
Han Jing <hanqijing@gmail.com>
2014-10-03 10:57:04 -0700

    update doc

--- mysqldbdump.sh
+++ mysqldbdump.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Simple script to do a db dump/backup of a specific database
-folder=~/backups/mysql/$(date -d "today" +"%Y-%m-%d")
+# Just change the "db_name" variable to get started
+#
 db_name=db_name
+
+folder=~/backups/mysql/$(date -d "today" +"%Y-%m-%d")
 mkdir -p $folder
 mysqldump -u root -p $db_name > $folder/$db_name.sql
